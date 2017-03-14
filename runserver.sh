23b0ff600990769c4521507de1def012aea64b98
jing wang <jingwangian@gmail.com>
2017-03-14 11:54:20 +1100

    add url in urls.py

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-python manage.py runserver
+if [ -e "website/local_settings.py" ]
+then
+    echo "cp website/settings.py website/settings_bak.py"
+    cp website/settings.py website/settings_bak.py
+    cp website/local_settings.py website/settings.py
+fi
+
+python manage.py runserver 0.0.0.0:8000
+
